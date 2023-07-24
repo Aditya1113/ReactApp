cb07c016cb886c7ac7019d214209951b95c41e38
Aditya Mazumdar <adityamazumdar@Adityas-MacBook-Air.local>
2023-07-24 20:15:46 +0530

    first

---
+++ src/MyApp.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyApp() {
+  return (
+    <div className="App">
+
+        <h1>Hello</h1>
+
+    </div>
+  );
+}
+
+export default MyApp;
