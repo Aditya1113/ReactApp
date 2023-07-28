d7bafe7b90d31720efc5dbcb3614217e2ec67a9a
Aditya Mazumdar <adityamazumdar@Adityas-MacBook-Air.local>
2023-07-29 03:29:07 +0530

    fixed app.js

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./App.css";
-
-import TodoMain from "./components/TodoList/TodoMain";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import About from "./components/Pages/About";
 import Menu from "./components/Menu";
-
 import UsersRoute from "./components/Pages/UsersRoute";
 import Posts from "./components/Pages/Posts";
 import Latest from "./components/Pages/Latest";
@@ -19,35 +15,36 @@ import EditPost from "./components/Pages/EditPost";
 import AddPost from "./components/Pages/AddPost";
 import UserLogin from "./components/Pages/UserLogin";
 import SignUp from "./components/Pages/SignUp";
+import TodoMain from "./components/TodoList/TodoMain";
 
 function MyApp() {
-  <BrowserRouter>
-    <Menu />
+  return (
+    <BrowserRouter>
+      <Menu />
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="posts" element={<Posts />}>
-        <Route path="latest" element={<Latest />} />
-        <Route path="oldest" element={<Oldest />} />
-        <Route path="liked" element={<Liked />} />
-        <Route path="disliked" element={<Disliked />} />
-      </Route>
-      <Route path="/about" element={<About />} />
-      <Route path="/todo" element={<TodoMain />} />
-      <Route path="/users" element={<UsersRoute />} />
-      {/* <Route path="*" element={<ViewNotFound/>}/> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="posts" element={<Posts />}>
+          <Route path="latest" element={<Latest />} />
+          <Route path="oldest" element={<Oldest />} />
+          <Route path="liked" element={<Liked />} />
+          <Route path="disliked" element={<Disliked />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/todo" element={<TodoMain />} />
+        <Route path="/users" element={<UsersRoute />} />
+        {/* <Route path="*" element={<ViewNotFound/>}/> */}
 
-      <Route path="/users/:id" element={<EditUser />} />
+        <Route path="/users/:id" element={<EditUser />} />
 
-      <Route path="/posts/latest/edit/:id" element={<EditPost />} />
-      {/* :id -> placeholder */}
-      <Route path="add" element={<AddPost />} />
-      <Route path="login" element={<UserLogin />} />
-      <Route path="signup" element={<SignUp />} />
-    </Routes>
-  </BrowserRouter>;
+        <Route path="/posts/latest/edit/:id" element={<EditPost />} />
+        {/* :id -> placeholder */}
+        <Route path="add" element={<AddPost />} />
+        <Route path="login" element={<UserLogin />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default MyApp;
-
-//props
